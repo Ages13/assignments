ea28ea3ffd8192d09d846b552cc8205d9974f8e6
David Ballard <davidball155@hotmail.com>
2022-08-19 15:12:17 -0400

    DJ JS

--- my-first-assignments/DJ-JS/DJ JS.js
+++ my-first-assignments/DJ-JS/DJ JS.js
@@ -1,18 +1,8 @@
-var bluebutton = document.getElementById("blue");
+/*var bluebutton = document.getElementById("blue");
 var redbutton = document.getElementById("red");
 var yellowbutton = document.getElementById("yellow");
 var greenbutton = document.getElementById("green");
 var orangebutton = document.getElementById("orange");
+*/
 
-bluebutton.addEventListener("mouseover", func, false);
-bluebutton.addEventListener("mouseout", func1, false);
-
-function func() {
-    bluebutton.setAttribute("style", "background-color:blue;")
-}
-
-function func1(){
-    bluebutton.setAttribute("style", "background-color:white;")
-
-}
-
+var squarebutton = document.getElementById("lazer")
