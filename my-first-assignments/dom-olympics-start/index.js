1daeea3a15ba551f4e8a97373f4d7ace57f14095
David Ballard <davidball155@hotmail.com>
2022-08-22 16:18:37 -0400

    Dom

---
+++ my-first-assignments/dom-olympics-start/index.js
@@ -0,0 +1,11 @@
+const h1 = document.createElement("h1");
+var span = document.createElement("span")
+h1.textContent = "Javascript made this!!";
+document.getElementById("header").appendChild(h1);
+
+
+var h2 = document.createElement('h2');
+h2.textContent = "David made this with Javascript"
+
+
+
