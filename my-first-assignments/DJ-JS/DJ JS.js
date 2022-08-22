a73c3025b6d7f3fff916a61d2389e4777d57ed3b
David Ballard <davidball155@hotmail.com>
2022-08-22 13:09:22 -0400

    DJ JS

--- my-first-assignments/DJ-JS/DJ JS.js
+++ my-first-assignments/DJ-JS/DJ JS.js
@@ -1,8 +1,65 @@
-/*var bluebutton = document.getElementById("blue");
-var redbutton = document.getElementById("red");
-var yellowbutton = document.getElementById("yellow");
-var greenbutton = document.getElementById("green");
-var orangebutton = document.getElementById("orange");
-*/
+let squarebutton = document.getElementById("square");
 
-var squarebutton = document.getElementById("lazer")
+squarebutton.addEventListener("mouseover", mouseOver);
+
+function mouseOver(){
+    console.log(mouseOver);
+    squarebutton.style.backgroundColor = "blue";
+}
+
+squarebutton.addEventListener("mousedown", mouseDown);
+
+function mouseDown(){
+    console.log(mouseDown);
+    squarebutton.style.backgroundColor = "red";
+}
+
+squarebutton.addEventListener("mouseup", mouseUp);
+
+function mouseUp(){
+    console.log(mouseUp);
+    squarebutton.style.backgroundColor = "yellow";
+}
+
+squarebutton.addEventListener("dblclick", dblClick, false);
+
+function dblClick(){
+    console.log(dblClick);
+    squarebutton.style.backgroundColor = "green";
+}
+
+squarebutton.addEventListener("wheel", wheel);
+
+function wheel(){
+    console.log(wheel);
+    squarebutton.style.backgroundColor  = "orange";
+}
+
+
+squarebutton.addEventListener("keydown", keyDown); 
+
+function keyDown() {
+    switch (event.keyCode) {
+        case 66:
+            setBackgroundColor("blue");
+            break;
+        case 82:
+            setBackgroundColor("red");
+            break;
+        case 89:
+            setBackgroundColor("yellow");
+            break;
+        case 71:
+            setBackgroundColor("green");
+            break;
+        case 79:
+            setBackgroundColor("orange");
+            break;
+    }
+
+};
+
+function setBackgroundColor(color) {
+  squarebutton.querySelector('body')
+    squarebutton.style.backgroundColor = color;
+}
