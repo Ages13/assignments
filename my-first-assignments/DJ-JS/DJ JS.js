35836182bd2d45124c91fee4489147ac77f62d76
David Ballard <davidball155@hotmail.com>
2022-08-25 14:09:11 -0400

    DJ JS

--- my-first-assignments/DJ-JS/DJ JS.js
+++ my-first-assignments/DJ-JS/DJ JS.js
@@ -1,3 +1,5 @@
+
+//Creates the hover color change
 let lazer = document.getElementById("square");
 
 lazer.addEventListener("mouseover", mouseOver);
@@ -6,28 +8,28 @@ function mouseOver(){
     console.log(mouseOver);
     lazer.style.backgroundColor = "blue";
 }
-
+//Creates the mouse click down color change
 lazer.addEventListener("mousedown", mouseDown);
 
 function mouseDown(){
     console.log(mouseDown);
     lazer.style.backgroundColor = "red";
 }
-
+//Creates the mouse up color change
 lazer.addEventListener("mouseup", mouseUp);
 
 function mouseUp(){
     console.log(mouseUp);
     lazer.style.backgroundColor = "yellow";
 }
-
+//Creates the double click color change
 lazer.addEventListener("dblclick", dblClick, false);
 
 function dblClick(){
     console.log(dblClick);
     lazer.style.backgroundColor = "green";
 }
-
+//Creates the scroll wheel color change..  Depending how much room is on the page, the scroll needs more room to work, so wheel was used
 lazer.addEventListener("wheel", wheel);
 
 function wheel(){
@@ -35,7 +37,7 @@ function wheel(){
     lazer.style.backgroundColor  = "orange";
 }
 
-
+//Allows the colors to be changed with the first letter of the color
 lazer.addEventListener("keydown", keyDown); 
 
 function keyDown() {
