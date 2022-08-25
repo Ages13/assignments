39ad445dc8efa494550494fb825635a4757ccf6e
David Ballard <davidball155@hotmail.com>
2022-08-25 13:53:30 -0400

    Moms shopping List

--- my-first-assignments/moms-shopping-list-start/index.js
+++ my-first-assignments/moms-shopping-list-start/index.js
@@ -1,16 +1,17 @@
 const myList = document.getElementById("list");
 
+// prevents the submit button form refreshing
 document.addItem.addEventListener("submit", function(event) {    
     event.preventDefault();
 
-
+//Creating the list and div to store the list items
 const listItem = addItem.title.value;
 var newLi = document.createElement("li");
 const newDiv = document.createElement("div");
 newDiv.textContent = listItem;
 newLi.appendChild(newDiv);
 
-
+// creating the edit and x/delete button and assigning it to an id
 const editButton = document.createElement('button');
 editButton.id = "edit";
 const xButton = document.createElement("button");
@@ -18,12 +19,13 @@ xButton.id = "x";
 editButton.textContent = "edit";
 xButton.textContent = "x";
 
+// adds the new buttons and list into the exisiting list
 newLi.appendChild(editButton);
 newLi.appendChild(xButton);
 myList.appendChild(newLi);
 
 })
-
+//takes the xButton and creates the event that allows it to remove a list item
 myList.addEventListener("click", function(e) {
     if (e.target.id === "x") {
         console.log(e.target);
@@ -33,6 +35,7 @@ myList.addEventListener("click", function(e) {
     }
 });
 
+//take the editButton and allows it to be edited
 myList.addEventListener("click", function(e) {
     const itemList = document.querySelector("ul div");
     if (e.target.id === "edit") {
